24481e7f809925285d166c5a715d68a4612064bf
Robert Greener <me@r0bert.dev>
2023-04-14 13:38:20 +0100

    Add methods to BehaviourDecisionAgent

--- contagent/src/main/java/xyz/contagent/behaviour/agents/behaviourdecision/BehaviourDecisionAgent.java
+++ contagent/src/main/java/xyz/contagent/behaviour/agents/behaviourdecision/BehaviourDecisionAgent.java
@@ -1,6 +1,19 @@
 package xyz.contagent.behaviour.agents.behaviourdecision;
 
+import org.jetbrains.annotations.NotNull;
+import xyz.contagent.behaviour.Behaviour;
+import xyz.contagent.behaviour.messages.chosenbehaviour.ChosenBehaviourMessage;
 import xyz.contagent.core.agents.Agent;
+import xyz.contagent.core.simulationmanager.SimulationManager;
+
+import java.util.UUID;
 
 public interface BehaviourDecisionAgent extends Agent {
+    Behaviour chooseBehaviour();
+
+    default void chooseAndInformChosenBehaviour(@NotNull SimulationManager simulationManager, @NotNull UUID destination) {
+        var behaviour = chooseBehaviour();
+        var message = new ChosenBehaviourMessage(null, this, destination, behaviour);
+        simulationManager.sendMessageNow(message);
+    }
 }
