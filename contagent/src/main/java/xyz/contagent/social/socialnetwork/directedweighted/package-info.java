8c7e2ab158a5421433fbcadd16d88bd0dc963fd3
Robert Greener <me@r0bert.dev>
2023-08-15 15:49:48 +0100

    Finish adding package-info.java to social package

---
+++ contagent/src/main/java/xyz/contagent/social/socialnetwork/directedweighted/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains a social network represented as a weighted digraph.
+ *
+ * @author Robert Greener
+ */
+package xyz.contagent.social.socialnetwork.directedweighted;
---
+++ contagent/src/main/java/xyz/contagent/social/socialnetwork/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains social networks.
+ *
+ * @author Robert Greener
+ */
+package xyz.contagent.social.socialnetwork;
